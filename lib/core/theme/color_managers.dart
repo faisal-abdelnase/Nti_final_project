f42d92dff8b250617b58aa27a93eedb3d460bb3b
faisal <faisalabdalnasser275@gmail.com>
2025-12-21 16:37:36 +0200

    add app theme

---
+++ lib/core/theme/color_managers.dart
@@ -0,0 +1,9 @@
+
+
+import 'package:flutter/material.dart';
+
+class ColorManagers {
+  static Color primaryColor = Color(0xff0F4888);
+  static Color secondryColor = Color(0xff03A9F4);
+
+}
---
+++ lib/core/theme/style_managers.dart
@@ -0,0 +1,139 @@
+
+import 'package:flutter/material.dart';
+import 'package:nti_final_project/core/theme/color_managers.dart';
+
+class StyleManagers {
+
+  static TextStyle font25PrimColor700 = TextStyle(
+    fontSize: 25,
+    color: ColorManagers.primaryColor,
+    fontWeight: FontWeight.w700,
+  );
+
+
+  static TextStyle font22PrimColor900 = TextStyle(
+    fontSize: 22,
+    color: ColorManagers.primaryColor,
+    fontWeight: FontWeight.bold,
+  );
+
+  static TextStyle font18PrimColor400 = TextStyle(
+    fontSize: 18,
+    color: ColorManagers.primaryColor,
+    fontWeight: FontWeight.w400,
+  );
+
+
+
+  static TextStyle font24White700 = TextStyle(
+    fontSize: 24,
+    color: Colors.white,
+    fontWeight: FontWeight.w700,
+  );
+
+
+
+  static TextStyle font16Black500 = TextStyle(
+    fontSize: 16,
+    color: Colors.black,
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle font16SecdColor500 = TextStyle(
+    fontSize: 16,
+    color: ColorManagers.secondryColor,
+    fontWeight: FontWeight.w500,
+  );
+
+
+  static TextStyle font18PrimColor600 = TextStyle(
+    fontSize: 18,
+    color: ColorManagers.primaryColor,
+    fontWeight: FontWeight.w600,
+  );
+
+
+
+  static TextStyle font35White700 = TextStyle(
+    fontSize: 35,
+    color: Colors.white,
+    fontWeight: FontWeight.bold,
+  );
+
+  static TextStyle font32White500 = TextStyle(
+    fontSize: 32,
+    color: Colors.white,
+    fontWeight: FontWeight.w500,
+  );
+
+
+
+  static TextStyle font20blue400 = TextStyle(
+    fontSize: 20,
+    color: Color(0xff1B526B),
+    fontWeight: FontWeight.w400,
+  );
+
+
+  static TextStyle font16Black700 = TextStyle(
+    fontSize: 16,
+    color: Colors.black,
+    fontWeight: FontWeight.w700,
+  );
+
+
+  static TextStyle font16BlackBlue700 = TextStyle(
+    fontSize: 16,
+    color: Color(0xff2C2D3A),
+    fontWeight: FontWeight.w700,
+  );
+
+
+  static TextStyle font39BlackBold = TextStyle(
+    fontSize: 39,
+    color: Colors.black,
+    fontWeight: FontWeight.bold,
+  );
+
+
+  static TextStyle font12White700 = TextStyle(
+    fontSize: 12,
+    color: Colors.white,
+    fontWeight: FontWeight.w700,
+  );
+
+  static TextStyle font12grey700 = TextStyle(
+    fontSize: 12,
+    color: Colors.grey,
+    fontWeight: FontWeight.w700,
+  );
+
+
+  static TextStyle font12White500 = TextStyle(
+    fontSize: 12,
+    color: Colors.white,
+    fontWeight: FontWeight.w500,
+  );
+
+
+  static TextStyle font16White400 = TextStyle(
+    fontSize: 16,
+    color: Colors.white,
+    fontWeight: FontWeight.w400,
+  );
+
+
+  static TextStyle font18white500 = TextStyle(
+    fontSize: 18,
+    color: Colors.white,
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle font18Red500 = TextStyle(
+    fontSize: 18,
+    color: Colors.red,
+    fontWeight: FontWeight.w500,
+  );
+
+
+}
